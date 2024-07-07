1355688dd6c7f6b47bf322f8b38f0703faf02cda
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-07 15:49:28 +1000

    Added default profile and error pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
-import Spinach from './components/Spinach';
-import Popeye from './components/Popeye';
+import ErrorPage from "./components/ErrorPage";
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: "profile",
+    path: "profile/:name",
     element: <Profile />,
-    children: [
-      {path: "spinach", element: <Spinach />},
-      {path: "popeye", element: <Popeye />},
-    ],
   },
 ]);
 
